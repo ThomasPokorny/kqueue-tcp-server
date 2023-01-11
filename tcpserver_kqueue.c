70c253a7857b3e74dbdd62159da13cbd750264e2
Thomas Pokorny <39346803+ThomasPokorny@users.noreply.github.com>
2023-01-11 22:19:59 +0100

    Updated event loop to echo clients msgs to stdout

--- tcpserver_kqueue.c
+++ tcpserver_kqueue.c
@@ -6,9 +6,11 @@
 #include <string.h>
 #include <unistd.h>
 
+#define ERROR_EXIT(...) { fprintf(stderr, "ERROR: " __VA_ARGS__); exit(EXIT_FAILURE); }
+
 int main()
 {
-    // All needed variables.
+    // Initialise all needed variables.
     int socket_listen_fd,
         portno = 1815,
         client_len,
@@ -20,14 +22,13 @@ int main()
     struct sockaddr_in serv_addr,
         client_addr;
 
-    // Create socket.
+    // Create socket
     if (((socket_listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0))
     {
-        perror("ERROR opening socket");
-        exit(1);
+      ERROR_EXIT("Error creating socket");
     }
 
-    // Create socket structure and bind to ip address.
+    // Bind to ip address
     bzero((char *)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
@@ -35,88 +36,72 @@ int main()
 
     if (bind(socket_listen_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
-        perror("Error binding socket");
-        exit(1);
+        ERROR_EXIT("Error binding socket");
     }
 
-    // Start listening.
+    // Mark socket as passive and start listening
     listen(socket_listen_fd, 3);
     client_len = sizeof(client_addr);
 
-    // Prepare the kqueue.
+    // Create kqueue
     kq = kqueue();
 
-    // Create event 'filter', these are the events we want to monitor.
-    // Here we want to monitor: socket_listen_fd, for the events: EVFILT_READ 
-    // (when there is data to be read on the socket), and perform the following
-    // actions on this kevent: EV_ADD and EV_ENABLE (add the event to the kqueue 
-    // and enable it).
+    // Create filter for events that we want to monitor on given fd
+    // one the EVFILT_READ event has occured, we add it to the event queue.
     EV_SET(change_event, socket_listen_fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, 0);
 
-    // Register kevent with the kqueue.
+    // Register events
     if (kevent(kq, change_event, 1, NULL, 0, NULL) == -1)
     {
-        perror("kevent");
-        exit(1);
+        ERROR_EXIT("kevent: register socket_listen_fd");
     }
 
-    // Actual event loop.
+    // Event Loop ⚛️
     for (;;)
     {
-        // Check for new events, but do not register new events with
-        // the kqueue. Hence the 2nd and 3rd arguments are NULL, 0.
-        // Only handle 1 new event per iteration in the loop; 5th
-        // argument is 1.
+        // Poll for new events
         new_events = kevent(kq, NULL, 0, event, 1, NULL);
         if (new_events == -1)
         {
-            perror("kevent");
-            exit(1);
+          ERROR_EXIT("kevent: poll for events");
         }
 
         for (int i = 0; new_events > i; i++)
         {
-            printf("amount of new events: %d\n", new_events);
             int event_fd = event[i].ident;
 
-            // When the client disconnects an EOF is sent. By closing the file
-            // descriptor the event is automatically removed from the kqueue.
+            // Close fd when client disconnects
             if (event[i].flags & EV_EOF)
             {
-                printf("Client has disconnected\n");
                close(event_fd);
             }
-            // If the new event's file descriptor is the same as the listening
-            // socket's file descriptor, we are sure that a new client wants 
-            // to connect to our socket.
+            // In case the events fd is the same as our listening fd
+            // we have an incoming connection
             else if (event_fd == socket_listen_fd)
             {
-                printf("New connection coming in...\n");    
+                printf("New connection coming in...\n");
 
-                // Incoming socket connection on the listening socket.
-                // Create a new socket for the actual connection to client.
                 socket_connection_fd = accept(event_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_len);
                 if (socket_connection_fd == -1)
                 {
-                    perror("Accept socket error");
+                  ERROR_EXIT("accept socket");
                 }
 
-                // Put this new socket connection also as a 'filter' event
-                // to watch in kqueue, so we can now watch for events on this
-                // new socket.
+                // Add the new connection to our interest list
                 EV_SET(change_event, socket_connection_fd, EVFILT_READ, EV_ADD, 0, 0, NULL);
                 if (kevent(kq, change_event, 1, NULL, 0, NULL) < 0)
                 {
-                    perror("kevent error");
+                    ERROR_EXIT("kevent: register socket_connection_fd");
                 }
             }
 
             else if (event[i].filter & EVFILT_READ)
             {
-                // Read bytes from socket
+                // Read from socket
                 char buf[1024];
                 size_t bytes_read = recv(event_fd, buf, sizeof(buf), 0);
-                printf("read %zu bytes\n", bytes_read);
+                buf[bytes_read] = '\0';
+                printf("read: %s\n", buf);
             }
         }
     }
